8dbcbc64a944b26ce50cf8cf8f3fd31a5f7c3f7f
begend <jackchen0227@sina.com>
2017-05-25 16:07:43 +0800

    Update fileSyncServer.go

--- fileSyncServer.go
+++ fileSyncServer.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"path/filepath"
 	"github.com/axgle/mahonia"
+	"io/ioutil"
+	"encoding/base64"
 )
 
 func fileExist(fileName string) bool{
@@ -87,8 +89,58 @@ func utf8ToGbk(str string) string {
 	return enc.ConvertString(str)
 }
 
+func getFile(w http.ResponseWriter, req *http.Request){
+	fileName := req.FormValue("fileName")
+
+	rootPath := req.FormValue("rootPath")
+
+	if len(rootPath) == 0 || len(fileName) == 0 {
+		fmt.Fprint(w, "no path")
+		return
+	}
+
+	code := "utf8"
+	if req.FormValue("code") != ""{
+		code = req.FormValue("code")
+	}
+	log.Println(fileName + " " + rootPath + " " + code)
+
+	filePath := fileName
+	dir, fileName := filepath.Split(filePath)
+	parentDir := filepath.Join(rootPath, dir)
+	if fileExist(parentDir){
+		log.Println(parentDir + " file exist")
+	}else {
+		err := os.MkdirAll(parentDir, 755)
+		log.Fatalln(err)
+	}
+
+	fullFileName := filepath.Join(parentDir, fileName)
+	var f *os.File
+	 f, err := os.Open(fullFileName)
+	 if err != nil {
+	  	log.Fatalln("open file failed " + fullFileName)
+	 }
+	buf, err := ioutil.ReadAll(f)
+
+	if code == "gbk"{
+		tmpStr := gbkToUtf8(string(buf))
+		buf = []byte(tmpStr)
+	}
+
+	resStr :=  base64.StdEncoding.EncodeToString(buf)
+	fmt.Fprint(w, resStr)
+
+	defer func(){
+		log.Println(rootPath + " " + fileName + " get")
+		if e := recover(); e != nil{
+			log.Fatal(e)
+		}
+	}()
+}
 func main() {
 	http.HandleFunc("/hello", HelloServer)
+	http.HandleFunc("/get", getFile)
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
